0be3c9a8c7b0f478d71ad16f57e2d1301a6d2e32
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-14 23:39:08 +0530

    subject pages setup

--- src/app/pages/4thsem/page.jsx
+++ src/app/pages/4thsem/page.jsx
@@ -6,27 +6,27 @@ const page = () => {
         <div className='w-full hero-bg'>
             <h1 className='text-center text-4xl font-semibold text-accent-dark hover:text-jacarta-900 pt-10'>Which Subject?</h1>
             <div className='container p-8  md:grid grid-cols-2 sm:grid-rows-1 font-semibold text-3xl gap-12 text-center'>
-                <Link href="./">
+                <Link href="./4thsem/DAA">
                     <div className=' p-8 rounded-3lg shadow-xl hover:shadow-2xl'>
                     Design and analysis of algorithms
                     </div>
                 </Link>
-                <Link href="./">
+                <Link href="./4thsem/CNC">
                     <div className=' p-8 rounded-3lg shadow-xl hover:shadow-2xl'>
                         Computer Networking and Communication
                     </div>
                 </Link>
-                <Link href="./">
+                <Link href="./4thsem/DBMS">
                     <div className=' p-8 rounded-3lg shadow-xl hover:shadow-2xl'>
                         Database system
                     </div>
                 </Link>
-                <Link href="./">
+                <Link href="./4thsem/MI">
                     <div className=' p-8 rounded-3lg shadow-xl hover:shadow-2xl'>
                         Microproccesor and Interfacing
                     </div>
                 </Link>
-                <Link href="./">
+                <Link href="./4thsem/MIT">
                     <div className=' p-8 rounded-3lg shadow-xl hover:shadow-2xl'>
                         Morality in Indian Tradition
                     </div>
